70f6039ff65a6417314d5b51fe99c606d5a4ee68
Vipin <fullstackdeveloper710@gmail.com>
2023-10-31 14:40:24 +0530

    working on admin ligin page

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
-import { Route, Routes } from "react-router";
-import { adminRoutes } from "./navigation/routes/adminRoutes";
-import AdPvtLayout from "./layouts/adminLayout/AdPvtLayout";
-import UsrPvtLayout from "./layouts/userLayout/usrPvtLayout";
-import { userRoutes } from "./navigation/routes/userRoutes";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./navigation";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<UsrPvtLayout />}>
-          {userRoutes.map(
-            ({ path, type, Auth, Component, defaultComp, id }) => {
-              console.log(path, "path here");
-              return (
-                <Route
-                  key={id}
-                  path={path}
-                  index={defaultComp}
-                  element={<Component />}
-                />
-              );
-            }
-          )}
-        </Route>
-
-        <Route path="/backoffice" element={<AdPvtLayout />}>
-          {adminRoutes.map(
-            ({ path, type, Auth, Component, defaultComp, id }) => {
-              console.log(path, "path here");
-              return (
-                <Route
-                  key={id}
-                  path={path}
-                  index={defaultComp}
-                  element={<Component />}
-                />
-              );
-            }
-          )}
-        </Route>
-      </Routes>
+      <Navigation />
     </>
   );
 }
---
+++ src/pages/admin/Login.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import { Button } from "../../components/user";
+import * as Yup from "yup";
+import { EyeLock } from "../../assets/icons/user";
+import { Form, Formik } from "formik";
+import "../../styles/admin/auth.scss";
+
+const AdminLogin = () => {
+  const [showPass, setShowPass] = useState(false);
+  const passwordRegExp =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+  const initValues = {
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email().required("required field"),
+    password: Yup.string()
+      .required("required field")
+      .matches(
+        passwordRegExp,
+        "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+      ),
+  });
+  function submit() {
+    console.log("login");
+  }
+
+  return (
+    <>
+      <section className="auth">
+        <div className="auth__inner">
+          <h1 className="auth__title">Login into the account</h1>
+          <Formik
+            initialValues={initValues}
+            validationSchema={validationSchema}
+            onSubmit={(values) => {
+              console.log(values, "values%%");
+            }}
+          >
+            {({
+              values,
+              isSubmitting,
+              errors,
+              touched,
+              handleChange,
+              handleSubmit,
+              handleBlur,
+            }) => (
+              <Form onSubmit={handleSubmit}>
+                <Row>
+                  <Col md={12}>
+                    <div className="inputRow">
+                      <input
+                        name="email"
+                        placeholder="E-mail"
+                        type="email"
+                        value={values.email}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                      />
+                      <span style={{ color: "red" }}>
+                        {errors.email && touched.email && errors.email}
+                      </span>
+                    </div>
+                  </Col>
+
+                  <Col md={12}>
+                    <div className="inputRow">
+                      <div className="inputRow__icon">
+                        <input
+                          name="password"
+                          placeholder="Enter Password"
+                          type={showPass ? "text" : "password"}
+                          value={values.password}
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                        />
+
+                        <span
+                          className="inputRow__iconGroup"
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setShowPass(!showPass);
+                          }}
+                        >
+                          {showPass ? <EyeLock /> : <EyeLock />}
+                        </span>
+                      </div>
+                      <span style={{ color: "red" }}>
+                        {errors.password && touched.password && errors.password}
+                      </span>
+                    </div>
+                  </Col>
+
+                  <Col md={12}>
+                    <div className="authRemember">
+                      <div className="remember-text">
+                        <input type="checkbox" id="remember" />
+                        <label htmlFor="remember">Remember Me</label>
+                      </div>
+
+                      <button
+                        className="forgotLink"
+                        onClick={() => window.open("/reset_password", "_self")}
+                      >
+                        Forgot Password?
+                      </button>
+                    </div>
+                  </Col>
+
+                  <Col md="12">
+                    <Button
+                      type="submit"
+                      title={"Login"}
+                      className="submitBtn"
+                      // submit={submit}
+                    />
+                  </Col>
+                </Row>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default AdminLogin;
