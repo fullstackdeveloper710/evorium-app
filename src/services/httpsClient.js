04816bb3516af9768663230a7073bae9a4fae341
Vipin <fullstackdeveloper710@gmail.com>
2023-11-01 11:49:03 +0530

    working on https requests

--- src/services/httpsClient.js
+++ src/services/httpsClient.js
@@ -1,6 +1,15 @@
 import axiosInstance from "./axiosInstance";
 const httpsClient = async (config) => {
-  return await axiosInstance(config);
+  console.log(config, "config here");
+  const configration = {
+    ...config,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer your_token_here", // Add your custom headers here
+      // Add more headers if needed
+    },
+  };
+  return await axiosInstance(configration);
 };
 
 export default httpsClient;
