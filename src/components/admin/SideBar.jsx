be6962c7ce325bc2cf83980c9d1b601273d919e7
Vipin <fullstackdeveloper710@gmail.com>
2023-11-02 18:00:39 +0530

    optimize admin sidebar

--- src/components/admin/SideBar.jsx
+++ src/components/admin/SideBar.jsx
@@ -1,41 +1,19 @@
 import React from "react";
 import { Image } from "react-bootstrap";
 import { logo } from "../../assets/images/admin";
-import {
-  about,
-  categories,
-  conditions,
-  dashboard,
-  faqs,
-  logout,
-  payment,
-  privacy,
-  programs,
-  sociallinks,
-  speakers,
-  support,
-  user,
-  tags,
-} from "../../assets/icons/admin";
+
 import "../../styles/admin/sidebar.scss";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { ROUTES } from "../../navigation/constants";
+import { adminSidebarList } from "../../utility/sidebarList";
 
 function SideBar() {
-  const {
-    adLogin,
-    adDashboard,
-    adUserDetail,
-    adUserList,
-    adProgramList,
-    adAddprogram,
-    adCategories,
-    adTags,
-    adPayment,
-    adSpeaker,
-    adFaq,
-    adFaqList,
-  } = ROUTES;
+  const location = useLocation();
+  const { pathname } = location;
+
+  const logoutHandler = () => {
+    console.log("logut function call");
+  };
   return (
     <div className="sidebar">
       <div className="brand-log">
@@ -46,90 +24,32 @@ function SideBar() {
 
       <div className="sidebar_links">
         <ul>
-          <li className="active">
-            <Link to={adDashboard}>
-              <Image src={dashboard} />
-              Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link to={adUserList}>
-              <Image src={user} />
-              Users
-            </Link>
-          </li>
-          <li>
-            <Link to={adSpeaker}>
-              <Image src={speakers} />
-              Speakers
-            </Link>
-          </li>
-          <li>
-            <Link to={adCategories}>
-              <Image src={categories} />
-              Categories
-            </Link>
-          </li>
-          <li>
-            <Link to={adTags}>
-              <Image src={tags} />
-              Tags
-            </Link>
-          </li>
-          <li>
-            <Link to={adProgramList}>
-              <Image src={programs} />
-              Programs
-            </Link>
-          </li>
-          <li>
-            <Link to={adPayment}>
-              <Image src={payment} />
-              Payments
-            </Link>
-          </li>
-          <li>
-            <Link to={adFaqList}>
-              <Image src={faqs} />
-              FAQ’s
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <Image src={sociallinks} />
-              Social Media Link’s
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <Image src={about} />
-              About Us
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <Image src={privacy} />
-              Privacy Policy
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <Image src={conditions} />
-              Terms & Conditions
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <Image src={support} />
-              Support
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <Image src={logout} />
-              Logout
-            </Link>
-          </li>
+          {adminSidebarList.map(
+            ({ id, path, title, icon, type, activeFor }, index) => {
+              if (type === "link") {
+                return (
+                  <li
+                    key={id}
+                    className={activeFor.includes(pathname) && "active"}
+                  >
+                    <Link to={path}>
+                      <Image src={icon} />
+                      {title}
+                    </Link>
+                  </li>
+                );
+              } else {
+                return (
+                  <li key={id} onClick={logoutHandler}>
+                    <Link to={path}>
+                      <Image src={icon} />
+                      {title}
+                    </Link>
+                  </li>
+                );
+              }
+            }
+          )}
         </ul>
       </div>
     </div>
