ba50f0e8b7e781e4d373f8b4288b7524db6383e2
Vipin <fullstackdeveloper710@gmail.com>
2023-11-07 11:37:06 +0530

    add apis end points

--- src/services/apiEndpoints.js
+++ src/services/apiEndpoints.js
@@ -1,7 +1,13 @@
 const userApi = {
   userSignup: "user/signup",
   usrLogin: "user/login",
-  usrPrograms : "user/programs",
+  usrPrograms: "user/programs",
+
+  //pending
+  usrVerify: "/user/verify_mobile_number",
+  usrRefreshToken: "/user/refresh_token",
+  usrForgetPass: "/user/forget_password",
+  usrResetPass: "/user/reset_password/:token",
 };
 
 const adminApi = {
@@ -17,6 +23,20 @@ const adminApi = {
   adAddCategory: "/admin/categories",
   adAddTags: "/admin/tags",
   adDashboard: "/admin/dashboard_stats",
+
+  //pending
+  adDelTag: "/admin/delete_tag/:id",
+  adDelUser: "/admin/delete_user/:id",
+  adDelProgram: "/admin/delete_program/:id",
+  adDelCategory: "/admin/delete_program/:id",
+  adDelSpeaker: "/admin/delete_speaker/:id",
+  adAddProgram: "/admin/program",
+  asGetProgram: "/admin/program/id",
+  adUpdateProgram: "/admin/program/id",
+  adSearchProgram: "/admin/search_program?query=learn&pageNo=1&pageSize=4",
+  adSearchUser: "/admin/search_user?query=search_string&pageNo=1&pageSize=4",
+  adFilterUser:
+    "/admin/filtered_users?pageNo=1&pageSize=10&startDate=2023-10-2&endDate=2023-10-26",
 };
 
 export { userApi, adminApi };
