70b5806417166b4b1f770fd3f8acfdd3b6d709d2
Vipin <vipen@surrealevents.com>
2023-11-22 18:00:16 +0530

    working on layouts

--- src/layouts/adminLayout/AdPubLayout.js
+++ src/layouts/adminLayout/AdPubLayout.js
@@ -1,7 +1,17 @@
-import React from "react";
-import { Outlet } from "react-router";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router";
+import { ROUTES } from "../../navigation/constants";
+import { useSelector } from "react-redux";
 
 const AdPubLayout = () => {
+  const { adminAuthtoken } = useSelector((state) => state.adAuth);
+  const navigate = useNavigate();
+  const { adDashboard } = ROUTES;
+  useEffect(() => {
+    if (adminAuthtoken) {
+      navigate(adDashboard);
+    }
+  }, [adminAuthtoken]);
   return (
     <div>
       <Outlet />
