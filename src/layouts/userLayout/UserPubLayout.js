f3c915e3736cf0decda7d55968fd047ed9bd07e0
Vipin <vipen@surrealevents.com>
2023-11-22 17:52:06 +0530

    working on foter comp

--- src/layouts/userLayout/UserPubLayout.js
+++ src/layouts/userLayout/UserPubLayout.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router";
-import { Header } from "../../components/user";
+import { FooterEvorium, Header } from "../../components/user";
 
 const UserPubLayout = () => {
   return (
     <div>
       <Header />
       <Outlet />
+      <FooterEvorium />
     </div>
   );
 };
